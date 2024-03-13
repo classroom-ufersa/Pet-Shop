3f568d8abf17ee94045e6227856d56fa4460d614
Kauê Carvalho <kauecarvalho2028@gmail.com>
2024-03-13 15:02:19 -0300

    Criar função que cadastra clientes

--- TAD.clientes/cliente.c
+++ TAD.clientes/cliente.c
@@ -10,7 +10,31 @@ char endereco[80];
 char animais[80];
 };
 
+Cliente * cria_cliente(char nome[], int telefone, char endereco, char animais){
 
+    Cliente * consumidor = (Cliente*) malloc(sizeof(Cliente*));
+    
+    if (consumidor == NULL ){
+        printf("Erro ao alocar memória!");
+        exit (1);
+    }
+
+    printf("Informe os dados do cliente: ");
+
+    printf("Nome do cliente: ");
+    scanf(" %[^\n]", consumidor->nome);
+
+    printf("Informe o numero de telefone do cliente: ");
+    scanf("%d", &consumidor->telefone);
+
+    printf("Informe o endereco do cliente: ");
+    scanf(" %[^\n]", consumidor->endereco);
+
+    printf("Informe qual seu animal: ");
+    scanf(" %[^\n]", consumidor->animais);
+
+    return consumidor;
+}
 
 
 
--- TAD.clientes/cliente.h
+++ TAD.clientes/cliente.h
@@ -1,3 +1,8 @@
 /* Definição de um novo tipo: Cliente
 */
 typedef struct cliente Cliente;
+
+/* Função que aloca memoria dinamicamente para um struct Aluno
+e recebe dados do cliente via teclado
+*/
+Cliente * cria_cliente(char nome[], int telefone, char endereco, char animais);
