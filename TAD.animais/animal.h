d80e082fb190b787212d995a9a47962e8338f635
AntonioErick <groot150x@gmail.com>
2024-03-17 16:45:03 -0300

    criaçao da funça vadastrar animal e alteraçoes na funçao add animal

--- TAD.animais/animal.h
+++ TAD.animais/animal.h
@@ -5,6 +5,10 @@ typedef struct animal{
     char nome[80];
     char especie[80];
     char saude[80];
+    int id;
 }Animal;
 
-void add_animal(Animal *pet, int total_de_pets, FILE *arquivo_petshop);
+
+void add_animal(Animal *pet, FILE *arquivo_petshop);
+
+Animal *cadastrar_animal(int total_de_pets, FILE *arquivo_petshop);
