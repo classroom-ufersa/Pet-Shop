8f09b045579c3967df22686e574e4254479fc6f7
Kauê Carvalho <kauecarvalho2028@gmail.com>
2024-03-25 14:27:34 -0300

    comit change

--- TAD.animais/animal.c
+++ TAD.animais/animal.c
@@ -2,62 +2,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void add_animal(Animal *pet, int total_de_pets, FILE *arquivo_petshop){
-
-    //Animal *pet = (Animal *)malloc(sizeof(Animal));
-
-    if (pet == NULL)
-    {
-        printf("Erro ao alocar memória!");
-        exit(1);
-    }
-
-    printf("Digite o nome do animal: ");
-    scanf(" %[^\n]", pet[total_de_pets].nome);
-
-    printf("Digite a especie do animal: ");
-    scanf(" %[^\n]", pet[total_de_pets].especie);
-
-    printf("Digite o estado de saude do animal: ");
-    scanf(" %[^\n]", pet[total_de_pets].saude);
-
-    //fprintf(arquivo_petshop, "Nome: %s\n", pet[total_de_pets].nome);
-    //fprintf(arquivo_petshop, "Especie: %s\n", pet[total_de_pets].especie);
-    //fprintf(arquivo_petshop, "Saude: %s\n", pet[total_de_pets].saude);
-    //fprintf(arquivo_petshop, "\n");
-
-}
-
-Animal *cadastra_animal(int num_animal)
-{
-    Animal *pet = (Animal *)malloc(num_animal * sizeof(Animal));
-
-    if (pet == NULL)
-    {
-        printf("Erro ao alocar memoria! ");
-        exit(1);
-    }
-
-   FILE *arquivo_petshop = fopen("petshop.txt", "a");
-    if (arquivo_petshop == NULL) {
-        printf("Erro ao abrir o arquivo!");
-        exit(1);
-    }
-
-    for (int i = 0; i < num_animal; i++) {
-        printf("\n Cadastro do animal %d\n", i+1);
-        add_animal(pet, i, arquivo_petshop);
-    }
-
-    fclose(arquivo_petshop);
-    return pet;
-}
-
-No2 *cria_no(Animal animal) {
+No2 *cria_no2(Animal animal) {
     No2 *Novo_no = (No2 *)malloc(sizeof(No2));
 
     if (Novo_no == NULL) {
-        printf("Erro ao alocar memória para novo nó;");
+        printf("Erro ao alocar memória para novo no;");
         exit(1);
     }
 
@@ -66,10 +15,10 @@ No2 *cria_no(Animal animal) {
     return Novo_no;
 }
 
-void insere_final(No2 **cabeca, Animal animal)
+void insere_final2(No2 **cabeca, Animal animal)
 {
 
-    No2 *Novo_no = cria_no(animal);
+    No2 *Novo_no = cria_no2(animal);
     if (*cabeca == NULL)
     {
         *cabeca = Novo_no;
@@ -85,4 +34,45 @@ void insere_final(No2 **cabeca, Animal animal)
 
         temp->proximo = Novo_no;
     }
+}
+
+Animal* add_animal(void){
+
+    Animal *pet = (Animal *)malloc(sizeof(Animal));
+
+    if (pet == NULL)
+    {
+        printf("Erro ao alocar memória!");
+        exit(1);
+    }
+
+    printf("Informe os dados do animal:\n");
+
+    printf("Digite o nome do animal: ");
+    scanf(" %[^\n]", pet->nome);
+
+    printf("Digite a especie do animal: ");
+    scanf(" %[^\n]", pet->especie);
+
+    printf("Digite o estado de saude do animal: ");
+    scanf(" %[^\n]", pet->saude);
+
+    return pet;
+}
+
+Animal *cadastra_animal(int num_animal)
+{
+    Animal *pet = (Animal *)malloc(num_animal * sizeof(Animal));
+
+    if (pet == NULL)
+    {
+        printf("Erro ao alocar memoria! ");
+        exit(1);
+    }
+
+    for (int i = 0; i < num_animal; i++) {
+        printf("\n Cadastro do animal\n");
+        pet[i] = *add_animal();
+    }
+    return pet;
 }
--- TAD.animais/animal.h
+++ TAD.animais/animal.h
@@ -1,20 +1,25 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-typedef struct animal{
+/* Definição da struct
+*/
+struct animal{
     char nome[80];
     char especie[80];
     char saude[80];
     int id;
-}Animal;
+};
 
+/* Definição de um novo tipo: Animal
+*/
+typedef struct animal Animal;
+
+/* Definicão de um novo tipo: No2
+*/
 typedef struct No {
     Animal animal;
     struct No *proximo;
 } No2;
 
 /* Esta função solicita ao usuário o nome, espécie e saúde de um animal e armazena esses dados no array pet.*/
-void add_animal(Animal *pet, int total_de_pets, FILE *arquivo_petshop);
+Animal* add_animal(void);
 
 /*Esta função aloca dinamicamente memória para um array de estruturas Animal com base no número fornecido em num_animal. 
 Em seguida, abre um arquivo chamado "petshop.txt" para adicionar.*/
