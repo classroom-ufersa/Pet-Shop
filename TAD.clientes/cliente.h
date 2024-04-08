1051c75e7e9b7e85927bf9947a2ca573c2750a05
Kauê Carvalho <kauecarvalho2028@gmail.com>
2024-04-07 22:37:50 -0300

    Comentando protótipos

--- TAD.clientes/cliente.h
+++ TAD.clientes/cliente.h
@@ -9,14 +9,19 @@ typedef struct Cliente {
     struct Cliente *prox;
 } Cliente;
 
+/* Função para adicionar um cliente*/
 void adicionarCliente(Cliente **listaClientes);
 
+/* Função para remover um cliente*/
 void removerCliente(Cliente **listaClientes);
 
+/* Função para buscar um cliente com base no seu nome*/
 Cliente *buscarClientePorNome(Cliente *listaClientes, const char *nome);
 
+/* Função para listar clientes e seus respectivos animais*/
 void listarClientes(Cliente *listaClientes);
 
+/* Função para liberar a memória alocada para os cliente se animais*/
 void liberarMemoria(Cliente *listaClientes);
 
 #endif
